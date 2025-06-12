630a0321030b34b7411adad20b915a7ea9e7a37e
Nicola Dimitrov <nikola.dimitrov3108@gmail.com>
2025-06-12 03:48:51 -0700

    Add news page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import Research from "@/components/Research";
 import Community from "@/components/Community";
+import News from "@/components/News";
 
 const Index = () => {
     return (
@@ -11,6 +12,7 @@ const Index = () => {
             <HeroSection />
             <Research />
             <Community />
+            <News />
         </div>
     );
 };
