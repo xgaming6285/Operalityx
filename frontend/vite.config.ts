74ed992b2d875e55df563dde68af2621a79b5a18
Nicola Dimitrov <nikola.dimitrov3108@gmail.com>
2025-07-29 06:47:40 -0700

    Remove pdf-js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "node_modules/pdfjs-dist/build/pdf.worker.min.mjs",
-          dest: "assets",
-        },
-      ],
-    }),
   ],
   resolve: {
     alias: {
