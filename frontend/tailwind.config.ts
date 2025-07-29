8c4ce17bdda4604cc20151d34d6ec5ef15546500
Nicola Dimitrov <nikola.dimitrov3108@gmail.com>
2025-07-29 06:27:16 -0700

    Revert "Make the footer mobile adjustable"
    This reverts commit 44f9f6e704bb1dcca28b403951d665229ac6d027.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -17,14 +17,6 @@ export default {
 				'2xl': '1400px'
 			}
 		},
-		screens: {
-			'xs': '475px',
-			'sm': '640px',
-			'md': '768px',
-			'lg': '1024px',
-			'xl': '1280px',
-			'2xl': '1536px',
-		},
 		extend: {
 			colors: {
 				border: 'hsl(var(--border))',
